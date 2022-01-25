c75a5c59cba38557cedf2e2089007e7153acb6f7
op7ic <3172590+op7ic@users.noreply.github.com>
2022-01-25 20:03:47 +0300

    update

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # LokiToWinEventLog
- Loki Scanner with output redirected to windows event log for easy incident response
+
+This repository containing script which allow [Loki](https://github.com/Neo23x0/Loki) to log to Windows Event Log so it can be collected for scaled up incident response.
+
+## What is Loki?
+
+From [author's webpage](https://github.com/Neo23x0/Loki):
+
+
+## Installation Instructions
+
+From an Admin Powershell console run ```.\Install.ps1```. 
+
+## Script Activities:
+
+This script does the following:
+
+* Creates the directory structure at ```c:\Program Files\Loki2WindowsEventLog```.
+* Write PowerShell code buffer to loki2wineventlog.ps1 in ```c:\Program Files\Loki2WindowsEventLog```.
+* Downloads Loki Scanner from GitHub ```https://github.com/Neo23x0/Loki/releases/download/v0.44.2/loki_0.44.2.zip```.
+* Sets up a scheduled task called ```LokiToWinEventLog``` to run the script every 3 hours and log to Windows Event Log called ```LokiEvents```.
+
+## loki2wineventlog.ps1 Activities:
+
+* Start Loki scanner using default rules.
+* Log output to CSV file located in ```c:\Program Files\Loki2WindowsEventLog\logs```.
+* Filter out INFO events and leave "NOTICE","ALERT","WARNING","RESULT" in for logging.
+* Parse resulting CSV file into Windows Event Log called ```LokiEvents```.
+
+## Output 
+
+![](./pic/output.png)
+
+![](./pic/rawoutput.png)
---
+++ install.ps1
@@ -0,0 +1,55 @@
+# Author: Jerzy 'Yuri' Kramarz
+# Create Program Files directories
+$Loki2WindowsEventLogDir = "$env:ProgramFiles\Loki2WindowsEventLog"
+$Loki2WindowsEventLogging = "$Loki2WindowsEventLogDir\logs"
+If(!(test-path $Loki2WindowsEventLogDir)) {
+  New-Item -ItemType Directory -Force -Path $Loki2WindowsEventLogDir
+}
+
+If(!(test-path $Loki2WindowsEventLogging)) {
+  New-Item -ItemType Directory -Force -Path $Loki2WindowsEventLogging
+}
+
+$lokizipPath = "$Loki2WindowsEventLogDir\loki.zip"
+if(!(test-path $lokizipPath)) {
+  # Requires TLS 1.2
+  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+  Invoke-WebRequest -Uri "https://github.com/Neo23x0/Loki/releases/download/v0.44.2/loki_0.44.2.zip" -OutFile "$lokizipPath"
+}
+Expand-Archive -Path $lokizipPath -DestinationPath $Loki2WindowsEventLogDir -Force
+
+$codebuffer = @'
+# Author: Jerzy 'Yuri' Kramarz
+# Setup our event log so Loki CSV events can go there directly. Ignore if already exist
+$logfileExists = Get-Eventlog -list | Where-Object {$_.logdisplayname -eq "LokiEvents"}
+if (! $logfileExists) {
+  New-EventLog -LogName LokiEvents -Source LokiEvents
+}
+
+$Loki2WindowsEventLogDir = "$env:ProgramFiles\Loki2WindowsEventLog"
+$Loki2WindowsEventLogging = "$Loki2WindowsEventLogDir\logs"
+$lokibin = "$Loki2WindowsEventLogDir\loki\loki.exe"
+$lokiupgrader = "$Loki2WindowsEventLogDir\loki\loki-upgrader.exe"
+$lokiparams = "--nolog --csv --nofilescan"
+$lokioutputcsv = "$Loki2WindowsEventLogging\lokioutput.csv"
+
+$proc = Start-Process -FilePath $lokibin -ArgumentList $lokiparams -RedirectStandardOut $lokioutputcsv -WindowStyle hidden -Passthru
+$proc.WaitForExit()
+
+$loki_csv_lines = Get-Content $lokioutputcsv
+
+Foreach ($item in $loki_csv_lines) {
+  $item = $(Write-Output $item  | Out-String -Width 1000 | Select-String -Pattern "NOTICE","ALERT","WARNING","RESULT")
+  Write-EventLog -LogName LokiEvents -Source LokiEvents -EntryType Information -EventId 1 -Message $item
+}
+'@
+$codebuffer | Out-File "$Loki2WindowsEventLogDir\loki2wineventlog.ps1"
+
+$shortPath = (New-Object -ComObject Scripting.FileSystemObject).GetFile("$Loki2WindowsEventLogDir\loki2wineventlog.ps1").ShortPath 
+# Setup reoccuring task for our execution of pesieve
+$TASK = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-WindowStyle hidden -nop -exec bypass $shortPath"
+$TRIGGER = New-ScheduledTaskTrigger -Once -At (Get-Date) -RepetitionInterval (New-TimeSpan -Minutes 180)
+$TASK_PERMISSIONS = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -RunLevel Highest -LogonType ServiceAccount
+Register-ScheduledTask -TaskName "LokiToWinEventLog" -Action $TASK -Trigger $TRIGGER -Principal $TASK_PERMISSIONS
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -Hidden -ExecutionTimeLimit (New-TimeSpan -Minutes 60) -RestartCount 1 -StartWhenAvailable
+Set-ScheduledTask -TaskName "LokiToWinEventLog" -Settings $settings
